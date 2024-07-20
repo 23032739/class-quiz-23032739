c36ca2e161dab35e08fac70af5bb6c9be3e50fd8
23032739 <163397290+23032739@users.noreply.github.com>
2024-07-20 19:35:08 +0200

    student grader

---
+++ Neon20.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Neon20 {
+   public static void main(String[] args)  {
+   int score;
+   Scanner dion = new Scanner(System.in);
+   System.out.println("Enter student score >>");
+   score = dion.nextInt();
+   if(score>=80){
+    System.out.println("Level 7(Distinction)");
+   }
+   else if(score>=70){
+    System.out.println("Level 6(Meritous achivement)");
+   }
+   else if(score>=60){
+    System.out.println("Level 5(Substantial achivement)");
+   }
+   else if(score>=50){
+    System.out.println("level 4(Moderate achievement)");
+   }
+   else if(score>=40){
+    System.out.println("Level 3(Adequate achievement)");
+   }
+   else if(score>=30){
+    System.out.println("level 2(Elementary achievement)");
+   }
+   else{
+    System.out.println("Level 1(Not achieved*Fail*)");
+   }
+   dion.close();
+   }
+}
